4b82e1b81afbe620f8dba39c1cd006a6de263a9c
Karan <pate1228@sheridancollege.ca>
2020-10-24 03:04:44 -0400

    Initial Commit, Finished landing page and theme. Todo: OnClick does not work so I tried to delete my whole project this is the second version still no fix.

---
+++ app/src/main/java/com/example/karanpatel_trivia/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.karanpatel_trivia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClick(v: View?){
+        if(v != null){
+            when(v.id){
+                btnPlay.id ->{
+
+                    }
+                }
+            }
+
+        }
+}
