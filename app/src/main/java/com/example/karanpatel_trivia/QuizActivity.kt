e5a3db0aeebb19148695c7f60499e9984956d1ed
Karan <pate1228@sheridancollege.ca>
2020-10-24 15:42:20 -0400

    Finished UI for the quiz screen using radio buttons and linear layout

---
+++ app/src/main/java/com/example/karanpatel_trivia/QuizActivity.kt
@@ -0,0 +1,11 @@
+package com.example.karanpatel_trivia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+}
