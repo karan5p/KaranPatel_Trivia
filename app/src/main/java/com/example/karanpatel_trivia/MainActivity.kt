136cdf1201ea16a0550eceb134f9523cd72a51c2
Karan <pate1228@sheridancollege.ca>
2020-10-24 12:20:37 -0400

    Fixed issue of no ui elements being accessible with gradle plugins

--- app/src/main/java/com/example/karanpatel_trivia/MainActivity.kt
+++ app/src/main/java/com/example/karanpatel_trivia/MainActivity.kt
@@ -3,21 +3,24 @@ package com.example.karanpatel_trivia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-    fun onClick(v: View?){
+    fun onClick(v:View?){
         if(v != null){
             when(v.id){
                 btnPlay.id ->{
-
-                    }
+                    this.getQuiz()
                 }
             }
-
         }
+    }
+    fun getQuiz(){
+
+    }
+
 }
